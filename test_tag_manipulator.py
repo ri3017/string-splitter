6dc16439aca0423bf4f8f6ae453ac2ba16cfc776
Ruxandra <istrateruxandra99@gmail.com>
2021-07-29 11:20:02 +0100

    new cut

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -1,27 +1,13 @@
-from tag_manipulator import TagManipulator
-
-def test_split_empty_string_result_empty_array():
+def test_split_one_string_result_array_of_one():
     # arrange
-    stringToSplit = ""
-    expResult = []
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
     result = None
     cut = TagManipulator()
 
     # act
-    result = cut.parse_string(stringToSplit)
-
-    # assert
-    assert result == expResult
-
-def test_split_comma_empty_string_result_empty_array():
-    # arrange
-    stringToSplit = ","
-    expResult = []
-    result = None
-    cut = TagManipulator()
-
-    # act
-    result = cut.parse_string(stringToSplit)
+    result = cut.parse_string(stringToSplit, regex)
 
     # assert
     assert result == expResult
