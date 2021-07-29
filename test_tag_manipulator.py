40170e94a1350c083fdd57f5382d75dc753d4aa5
Ruxandra <istrateruxandra99@gmail.com>
2021-07-29 11:40:55 +0100

    test split two string result array of two

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -1,8 +1,50 @@
+from tag_manipulator import TagManipulator
+
 def test_split_one_string_result_array_of_one():
     # arrange
     stringToSplit = "java"
     regex = ","
     expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+def test_split_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ""
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+def test_split_two_string_result_array_of_two():
+    # arrange
+    stringToSplit = "java, python"
+    regex = ","
+    expResult = ["java", "python"]
     result = None
     cut = TagManipulator()
 
